04c156658e32398cddf1cad29907d5cf8032c75a
DorukUlucay <doruk36@yahoo.com>
2016-07-14 14:45:05 +0300

    add the working code

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace migrator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceQuery = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.targetTable = new System.Windows.Forms.TextBox();
+            this.generatedQuery = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConnStr = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCopyToClipboard = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sourceQuery
+            // 
+            this.sourceQuery.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sourceQuery.Location = new System.Drawing.Point(108, 94);
+            this.sourceQuery.Multiline = true;
+            this.sourceQuery.Name = "sourceQuery";
+            this.sourceQuery.Size = new System.Drawing.Size(506, 277);
+            this.sourceQuery.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source Query";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Target Table";
+            // 
+            // targetTable
+            // 
+            this.targetTable.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.targetTable.Location = new System.Drawing.Point(108, 396);
+            this.targetTable.Name = "targetTable";
+            this.targetTable.Size = new System.Drawing.Size(506, 28);
+            this.targetTable.TabIndex = 3;
+            this.targetTable.Enter += new System.EventHandler(this.targetTable_Enter);
+            // 
+            // generatedQuery
+            // 
+            this.generatedQuery.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generatedQuery.Location = new System.Drawing.Point(148, 21);
+            this.generatedQuery.Multiline = true;
+            this.generatedQuery.Name = "generatedQuery";
+            this.generatedQuery.Size = new System.Drawing.Size(506, 277);
+            this.generatedQuery.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtConnStr);
+            this.groupBox1.Controls.Add(this.lblMessage);
+            this.groupBox1.Controls.Add(this.btnGenerate);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.sourceQuery);
+            this.groupBox1.Controls.Add(this.targetTable);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(659, 691);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            this.groupBox1.Leave += new System.EventHandler(this.groupBox1_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 34);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Connection \r\nString";
+            // 
+            // txtConnStr
+            // 
+            this.txtConnStr.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConnStr.Location = new System.Drawing.Point(108, 43);
+            this.txtConnStr.Name = "txtConnStr";
+            this.txtConnStr.Size = new System.Drawing.Size(506, 28);
+            this.txtConnStr.TabIndex = 9;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblMessage.Location = new System.Drawing.Point(9, 559);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 29);
+            this.lblMessage.TabIndex = 5;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(108, 456);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(212, 68);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate Insert Code";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCopyToClipboard);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.generatedQuery);
+            this.groupBox2.Location = new System.Drawing.Point(679, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(660, 691);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // btnCopyToClipboard
+            // 
+            this.btnCopyToClipboard.Location = new System.Drawing.Point(148, 345);
+            this.btnCopyToClipboard.Name = "btnCopyToClipboard";
+            this.btnCopyToClipboard.Size = new System.Drawing.Size(212, 68);
+            this.btnCopyToClipboard.TabIndex = 6;
+            this.btnCopyToClipboard.Text = "Copy to clipboard";
+            this.btnCopyToClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipboard_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Generated Query";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1351, 715);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourceQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox targetTable;
+        private System.Windows.Forms.TextBox generatedQuery;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCopyToClipboard;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConnStr;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.IO;
+
+namespace migrator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("settings.txt"))
+            {
+                TextReader reader = File.OpenText("settings.txt");
+                string settings = reader.ReadToEnd();
+                txtConnStr.Text = settings;
+            }
+
+            sourceQuery.Text = "SELECT * FROM ";
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(txtConnStr.Text);
+
+            using (con)
+            {
+                SqlDataAdapter da = new SqlDataAdapter(sourceQuery.Text, con);
+                DataTable table = new DataTable();
+                da.Fill(table);
+
+                string q = "INSERT INTO " + targetTable.Text + "({0}) VALUES({1});";
+                string finalQ = "";
+
+                foreach (DataRow row in table.Rows)
+                {
+                    string COLS = "";
+                    string VALS = "";
+
+                    for (int i = 0; i < row.ItemArray.Length; i++)
+                    {
+                        COLS += "\"" + row.Table.Columns[i].ColumnName + "\",";
+
+                        if (row.Table.Columns[i].ColumnName == "CREATETIME"
+                            || row.Table.Columns[i].ColumnName == "UPDATETIME")
+                        {
+
+                            VALS += "sysdate,";
+
+                        }
+                        else if (row.Table.Columns[i].ColumnName == "CREATEUSERID" || row.Table.Columns[i].ColumnName == "UPDATEUSERID")
+                        {
+                            VALS += "1,";
+                        }
+                        else
+                        {
+                            if (row.Table.Columns[i].DataType == typeof(DateTime) || row.Table.Columns[i].DataType == typeof(string))
+                            {
+                                if (row[i] == null)
+                                {
+                                    VALS += "NULL,";
+                                }
+                                else
+                                {
+                                    string val = row[i].ToString().Contains("'") ? row[i].ToString().Replace("'", "''") : row[i].ToString();
+                                    VALS += "'" + val + "'" + ",";
+                                }
+                            }
+                            else if (row.Table.Columns[i].DataType == typeof(int))
+                            {
+                                if (row[i] == null)
+                                {
+                                    VALS += "0,";
+                                }
+                                else
+                                {
+                                    VALS += row[i] + ",";
+                                }
+                            }
+                            else//bool
+                            {
+                                if (row[i].ToString() == "True")
+                                {
+                                    VALS += "1,";
+                                }
+                                else
+                                {
+                                    VALS += "0,";
+                                }
+                            }
+                        }
+                    }
+
+
+                    COLS = COLS.TrimEnd(',');
+                    VALS = VALS.TrimEnd(',');
+
+                    finalQ += "\r\n" + string.Format(q, COLS, VALS);
+                }
+
+                generatedQuery.Text = finalQ;
+                sourceQuery.Text = "SELECT * FROM ";
+                targetTable.Text = "";
+                lblMessage.Visible = true;
+                lblMessage.Text = "done!";
+            }
+        }
+
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(generatedQuery.Text);
+        }
+
+        private void groupBox1_Leave(object sender, EventArgs e)
+        {
+            lblMessage.Visible = false;
+        }
+
+        private void targetTable_Enter(object sender, EventArgs e)
+        {
+            targetTable.Text = 
+                sourceQuery.Text.Split(
+                    new string[] { "FROM" }, 
+                    StringSplitOptions.RemoveEmptyEntries)[1]
+                    .Split(
+                    new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)[0];
+        }
+    }
+}
